0f3197b9d61c8c5d58fecc21242b4b75541d94c2
Santiago Blas Beitia <santiagob.beitia@gmail.com>
2018-11-20 15:49:54 -0300

    Modulos, export y math.js

---
+++ Modulos/index.js
@@ -0,0 +1,9 @@
+const Math = require("./math.js");
+
+console.log(Math);
+
+/* console.log(Math.add(1,2));
+console.log(Math.substract(4,6));
+console.log(Math.multiply(2,3));
+console.log(Math.divide(2,0));
+console.log(Math.divide(4,3)); */
